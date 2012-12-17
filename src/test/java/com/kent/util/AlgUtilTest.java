026d772f846f891836f94ffba57e5a30b60e49e4
Kai Yuan <kent.yuan@Gmail.com>
2012-12-17 23:30:01 +0100

    add SOansers package. could be removed later

--- src/test/java/com/kent/util/AlgUtilTest.java
+++ src/test/java/com/kent/util/AlgUtilTest.java
@@ -1,5 +1,10 @@
 package com.kent.util;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,6 +12,7 @@ import org.junit.Test;
  * The Class AlgUtilTest.
  */
 public class AlgUtilTest {
+	protected static final Logger log = Logger.getLogger(AlgUtilTest.class);
 
 	/**
 	 * Gets the random int array test.
@@ -63,4 +69,50 @@ public class AlgUtilTest {
 		Assert.assertArrayEquals(b, resultB);
 	}
 
+	@Test
+	public void testIt2() {
+		final String s = "My name is not eugene. my pet name is not eugene.";
+		final String tmp = s.toLowerCase().replaceAll("[^a-zA-Z]", " ");// here we need the placeholder (blank)
+		final String w1 = "my "; // leave a blank at the end to avoid those words e.g. "myself", "myth"..
+		final String w2 = "eugene ";// same as above
+		final List<Integer> l1 = getList(tmp, w1); // indexes list
+		final List<Integer> l2 = getList(tmp, w2);
+		int min = Integer.MAX_VALUE;
+		final int[] idx = new int[] { 0, 0 };
+
+		// loop to find out the result
+		for (final int i : l1) {
+			for (final int j : l2) {
+				if (Math.abs(j - i) < min) {
+					final int x = j - i;
+					min = Math.abs(j - i);
+					idx[0] = j - i > 0 ? i : j;
+					idx[1] = j - i > 0 ? j + w2.length() + 2 : i + w1.length() + 2;
+				}
+			}
+
+		}
+
+		System.out.println("indexes: " + Arrays.toString(idx));
+		System.out.println("result: " + s.substring(idx[0], idx[1]));
+	}
+
+	private List<Integer> getList(final String input, final String search) {
+		String t = new String(input);
+		final List<Integer> list = new ArrayList<Integer>();
+		int tmp = 0;
+		while (t.length() > 0) {
+			final int x = t.indexOf(search);
+
+			if (x < 0 || x > t.length()) {
+				break;
+			}
+			tmp += x;
+			list.add(tmp);
+			t = t.substring(search.length() + x);
+
+		}
+		return list;
+
+	}
 }
