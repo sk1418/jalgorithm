48cbe6cabed24b9bb78a866d9dd068a6bfc0f133
Kai Yuan <kent.yuan@Gmail.com>
2013-04-22 00:27:30 +0200

    add TwoSumInArray class, without implementaion

---
+++ src/main/java/com/kent/algorithm/problem/TwoSumInArray.java
@@ -0,0 +1,43 @@
+package com.kent.algorithm.problem;
+
+import com.kent.algorithm.Problem;
+
+/**
+ * <p>
+ * Given an array of integers, find two numbers such that they add up to a specific target number.
+ * </p>
+ * <p>
+ * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please
+ * note that your returned answers (both index1 and index2) are not zero-based.
+ * </p>
+ * 
+ * 
+ * <p>
+ * You may assume that each input would have exactly one solution.
+ * </p>
+ * 
+ * <pre>
+ *  Input: numbers={2, 7, 11, 15}, target=9
+ *  Output: index1=1, index2=2
+ * </pre>
+ * 
+ * 
+ * @author kent
+ */
+public class TwoSumInArray extends Problem<int[], int[]> {
+
+	/** {@inheritDoc} */
+	@Override
+	public String getBigO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public int[] solve(final int[] data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
