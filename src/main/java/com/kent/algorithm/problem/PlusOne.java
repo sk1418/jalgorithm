17d69b990a62c44cede3e80e30bbdbbb3488580a
Kai Yuan <kent.yuan@Gmail.com>
2013-05-03 00:18:08 +0200

    add PlusOne problem

---
+++ src/main/java/com/kent/algorithm/problem/PlusOne.java
@@ -0,0 +1,41 @@
+package com.kent.algorithm.problem;
+
+import java.util.Arrays;
+
+import com.kent.algorithm.Problem;
+
+/**
+ * <p>
+ * Given a number represented as an array of digits, plus one to the number.
+ * </p>
+ * 
+ * @author kent
+ * 
+ */
+public class PlusOne extends Problem<int[], int[]> {
+
+	@Override
+	public String getBigO() {
+		return "O(n)";
+	}
+
+	@Override
+	public int[] solve(final int[] data) {
+		final int carry = 1;
+		for (int i = data.length - 1; i >= 0; i--) {
+			if (data[i] < 9) {
+				data[i] += carry;
+				return data;
+			} else {
+				data[i] = 0;
+			}
+		}
+
+		// if reach here, means all elements in array are 9
+		final int[] result = Arrays.copyOf(data, data.length + 1);
+		result[result.length - 1] = 0;
+		result[0] = 1;
+		return result;
+	}
+
+}
