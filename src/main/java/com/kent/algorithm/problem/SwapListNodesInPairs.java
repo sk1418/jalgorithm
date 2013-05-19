c1fb4c7ff14120e95b88b57245161e96353d8ffe
Kai Yuan <kent.yuan@Gmail.com>
2013-05-19 02:22:45 +0200

    add Problem SwapListNodesInPairs

---
+++ src/main/java/com/kent/algorithm/problem/SwapListNodesInPairs.java
@@ -0,0 +1,82 @@
+package com.kent.algorithm.problem;
+
+import com.kent.algorithm.Problem;
+import com.kent.datastructure.ListNode;
+
+/**
+ * <p>
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * 
+ * For example
+ * 
+ * <pre>
+ * {@code
+ * [1->2->3->4] to [2->1->4->3]  
+ * }</code> .
+ * 
+ * Your algorithm should use only <b>constant</b> space. You may not modify the values in the list, only nodes itself can be changed.
+ * 
+ * </p>
+ * 
+ * <b>Solution/Algorithm</b>
+ * 
+ * <pre>
+ *  current processing node: curr= b
+ * 
+ *    ....-> a -> b -> c -> d ...
+ * 
+ *    ....-> a    b -> c -> d ...
+ *           |         ^
+ *           +---------|
+ * 
+ *                +---------+
+ *                |         v
+ *    ....-> a    b    c -> d ...
+ *           |         ^
+ *           +---------|
+ * 
+ *                +---------+
+ *                |         v
+ *    ....-> a    b <- c    d ...
+ *           |         ^
+ *           +---------|
+ *    
+ *    ....-> a -> c -> b -> d ...       
+ * 
+ *    curr = b.next (d)
+ * </pre>
+ * 
+ * @author kent
+ * 
+ */
+public class SwapListNodesInPairs extends Problem<ListNode, ListNode> {
+
+	@Override
+	public String getBigO() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ListNode solve(ListNode data) {
+		ListNode nn; // next of next
+		ListNode n; // next node
+		ListNode pre = null; // previous node
+		final ListNode head = data != null && data.next != null ? data.next : data; // get head to be returned
+		while (data != null && data.next != null) {
+			nn = data.next.next;
+			n = data.next;
+			if (pre != null) {
+				pre.next = n;
+			}
+			data.next = nn;
+			n.next = data;
+			pre = data;
+			data = data.next;
+
+		}
+
+		return head;
+	}
+
+}
