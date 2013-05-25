f13aa6c94c7feb09259c1094c4eaa7c6b94ddc61
Kai Yuan <kent.yuan@Gmail.com>
2013-05-25 02:16:11 +0200

    add RemoveNthNodeFromEndOfLinkedList and unittest

---
+++ src/main/java/com/kent/algorithm/problem/RemoveNthNodeFromEndOfLinkedList.java
@@ -0,0 +1,66 @@
+package com.kent.algorithm.problem;
+
+import java.util.Map;
+
+import com.kent.algorithm.Problem;
+import com.kent.datastructure.ListNode;
+
+/**
+ * 
+ * <p>
+ * Given linked list:
+ * 
+ * <pre>
+ *   1->2->3->4->5, and n = 2.
+ * </pre>
+ * 
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * 
+ * <pre>
+ * Note:
+ * Given n will always be valid.
+ * Try to do this in one pass.
+ * </pre>
+ * 
+ * </p>
+ * 
+ * 
+ * <b> Algorithm/Solution </b>
+ * <p>
+ * use two pointers,fast pointer forward n steps first
+ * </p>
+ * 
+ * @author kent
+ * 
+ */
+public class RemoveNthNodeFromEndOfLinkedList extends Problem<Map<String, Object>, ListNode> {
+
+	@Override
+	public String getBigO() {
+		return "O(n)";
+	}
+
+	@Override
+	public ListNode solve(final Map<String, Object> data) {
+		final ListNode head = (ListNode) data.get("head");
+		int n = (Integer) data.get("n");
+
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while (fast.next != null) {
+			fast = fast.next;
+			if (n-- <= 0) {
+				slow = slow.next;
+			}
+		}
+
+		if (slow == head) {
+			return head.next;
+		}
+		slow.next = slow.next.next;
+		return head;
+
+	}
+
+}
---
+++ src/test/java/com/kent/algorithm/problem/RemoveNthNodeFromEndOfLinkedListTest.java
@@ -0,0 +1,64 @@
+package com.kent.algorithm.problem;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+import com.kent.datastructure.ListNode;
+import com.kent.test.BaseTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The Class FindHeavyIntegerTest.
+ */
+public class RemoveNthNodeFromEndOfLinkedListTest extends BaseTest {
+
+	/** The Constant LOG. */
+	private static final Logger LOG = LoggerFactory.getLogger(RemoveNthNodeFromEndOfLinkedListTest.class);
+
+	final ListNode n1 = new ListNode(1);
+	final ListNode n2 = new ListNode(2);
+	final ListNode n3 = new ListNode(3);
+	final ListNode n4 = new ListNode(4);
+	final ListNode n5 = new ListNode(5);
+
+	@Before
+	public void initList() {
+
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+
+	}
+
+	/**
+	 * Solution test.
+	 */
+	@Test
+	public void solutionTest() {
+		testInfo("Remove the nth node from end of linkedlist");
+
+		final RemoveNthNodeFromEndOfLinkedList problem = new RemoveNthNodeFromEndOfLinkedList();
+
+		final Map<String, Object> input = Maps.newHashMap();
+		input.put("head", n1);
+		input.put("n", 2);
+
+		ListNode result = problem.solve(input);
+
+		assertEquals("[1 -> 2 -> 3 -> 5]", result.toString());
+
+		initList();
+		input.put("n", 5);
+		result = problem.solve(input);
+		assertEquals("[2 -> 3 -> 4 -> 5]", result.toString());
+
+	}
+}
