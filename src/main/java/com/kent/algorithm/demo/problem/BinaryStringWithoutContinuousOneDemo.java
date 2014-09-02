26a063aa634a11e59b21e0660ed9fc9bed464417
Kent@LsyHP <kent.yuan@Gmail.com>
2014-09-02 18:54:51 +0200

    add javadoc and demo description for BinaryStringWithoutContinuousOne

--- src/main/java/com/kent/algorithm/demo/problem/BinaryStringWithoutContinuousOneDemo.java
+++ src/main/java/com/kent/algorithm/demo/problem/BinaryStringWithoutContinuousOneDemo.java
@@ -1,5 +1,6 @@
 package com.kent.algorithm.demo.problem;
 
+import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.kent.algorithm.Problem;
@@ -31,6 +32,30 @@ public class BinaryStringWithoutContinuousOneDemo extends AbstractDemo {
 
     @Override
     protected String demoDescription() {
-        return null;
+        return Joiner.on("\n").join(
+            "  Given an integer N, print the count of all binary representations, which has length N and no continuous 1s.",
+            "  For example:",
+            "  ",
+            "      Given N=2, output Result=3 (00, 01, 10)",
+            "      Given N=3, output Result=5 (000,001,010,100,101)",
+            "  ",
+            "      Solution:",
+            " ",
+            "      we have two different kind of string, ending with 1 or 0.",
+            "      let's say:",
+            "      ",
+            "      zero[i] is the count of those string which has length=i and with 0 at the end",
+            "      one[i] is the count of those string which has length=i and with 0 at the end",
+            " ",
+            "      for getting [i+1] counts, zero[i] could be appended by 0 or 1, but one[i] could only be appended by 0",
+            " ",
+            "      that is:",
+            "      zero[i] = zero[i-1] + one[i-1]",
+            "      one[i] = zero[i-1];",
+            " ",
+            "      Note",
+            "      in fact the count would be a Fibonacci sequence. since",
+            "      zero[i]+one[i] = zero[i] + zero[i-1]"
+        );
     }
 }
--- src/main/java/com/kent/algorithm/problem/BinaryStringWithoutContinuousOne.java
+++ src/main/java/com/kent/algorithm/problem/BinaryStringWithoutContinuousOne.java
@@ -3,7 +3,38 @@ package com.kent.algorithm.problem;
 import com.kent.algorithm.Problem;
 
 /**
- * @author: Kai Yuan
+ * <p>
+ * Given an integer N, print the count of all binary representations, which has length N and no continuous 1s.
+ * For example:
+ * <pre>
+ *     Given N=2, output Result=3 (00, 01, 10)
+ *     Given N=3, output Result=5 (000,001,010,100,101)
+ * </pre>
+ * </p>
+ *
+ * <p>
+ *     Solution:
+ *
+ *     we have two different kind of string, ending with 1 or 0.
+ *     let's say:
+ *     <pre>
+ *     zero[i] is the count of those string which has length=i and with 0 at the end
+ *     one[i] is the count of those string which has length=i and with 0 at the end
+ *
+ *     for getting [i+1] counts, zero[i] could be appended by 0 or 1, but one[i] could only be appended by 0
+ *
+ *     that is:
+ *     zero[i] = zero[i-1] + one[i-1]
+ *     one[i] = zero[i-1];
+ *
+ *     <b>Note</b>
+ *     in fact the count would be a Fibonacci sequence. since
+ *     zero[i]+one[i] = zero[i] + zero[i-1]
+ *     </pre>
+ *
+ *
+ * </p>
+ * @author: Kent
  * Created on 2014-08-27.
  */
 public class BinaryStringWithoutContinuousOne extends Problem<Integer, Integer> {
