47b5f6a4d9aa63fa5f98f8be66bc55d70a140026
Kai Yuan <kent.yuan@Gmail.com>
2014-09-05 22:39:08 +0200

    add unittest

--- src/test/java/com/kent/algorithm/problem/PrintNumInAlphabetOrderTest.java
+++ src/test/java/com/kent/algorithm/problem/PrintNumInAlphabetOrderTest.java
@@ -2,6 +2,7 @@ package com.kent.algorithm.problem;
 
 import com.kent.algorithm.Problem;
 import com.kent.test.BaseTest;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -15,6 +16,7 @@ public class PrintNumInAlphabetOrderTest extends BaseTest{
 
     @Test
     public void testSolution() {
-        log.debug(Arrays.toString(problem.solve(20)));
+        int[] expected = new int[]{1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 3, 30, 31, 32, 33, 34, 35, 36, 37, 4, 5, 6, 7, 8, 9};
+        Assert.assertArrayEquals(expected, problem.solve(37));
     }
 }
